8e3484490ae63c0a5c01ef03c69232cce9e1deab
axolotliterature <andrewhansen.dev@gmail.com>
2023-02-02 16:21:26 -0800

    canvas render loads pdf, added css
    basic pdf render in canvas with buttons for page selection, added css to style and center pdf

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let pdf; //stores pdf data
 let canvas; //renders pdf file
+let viewport;
 let isPageRendering;
 let pageRenderingQueue = null; //next page number to render
-let canvasContext;
+let canvasContext; //ctx
 let totalPages;
 let currentPageNum = 1;
 
-window.addEventListener('load', function () {
-    isPageRendering = false;
-    pageRenderingQueue = null;
-    canvas = document.getElementById('pdf_canvas');
-    canvasContext = canvas.getContext('2d');
+let pdfState = {
+    pdf: null,
+    currentPage: 1,
+    zoom: 1
+}
 
-    initEvents(); Add events
-    initPDFRenderer(); //renders first page
+pdfjsLib.getDocument('./my_Document.pdf').then((pdf) => {
+    pdfState.pdf = pdf;
+    render();
+});
 
-)}
+function render () {
+    pdfState.pdf.getPage(pdfState.currentPage).then((page) => {
+        canvas = document.getElementById("pdfCanvas");
+        canvasContext = canvas.getContext('2d');
+
+        viewport = page.getViewport(pdfState.zoom);
+
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+
+        page.render({
+            canvasContext: canvasContext,
+            viewport: viewport
+        });
+    });
+}
